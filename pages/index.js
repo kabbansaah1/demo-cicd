aed4d72f49daa91af10cb23ba27cdd4a2a9526b1
Kobina Abban-Saah <kabbansaah1@babson.edu>
2023-09-05 15:00:23 -0700

    2

--- pages/index.js
+++ pages/index.js
@@ -44,7 +44,7 @@ const getLocation = (location) =>
         </p>
         <Link href="/locations/[id]" as={`/locations/${location.id}`}>
             <a className="btn btn-secondary">
-                Learn more!!!23
+                Learn more!!!2BUCKLE MYSHOE
             </a>
         </Link>
     </div>;  
